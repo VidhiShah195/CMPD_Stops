06d3c020455c46c21e5a10322db50a4a01497d87
thaouyen9112 <thaouyn9112@gmail.com>
2024-04-08 19:45:58 -0400

    new chart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # conda activate streamlit_2
 # pip freeze so you freeze the version of the packages, stremlit and python so someone can run it 10 years later and get the same results. 
 import streamlit as st
+import altair as alt
 import pandas as pd
 
 st.write('CMPD Traffic Stops')
@@ -14,4 +15,14 @@ def load_data(csv):
 
 stops = load_data("data/Officer_Traffic_Stops.csv")
 
-st.dataframe(stops)
+chart = (
+    alt.Chart(stops).mark_bar().encode(
+        alt.X("Driver_Age", bin=alt.Bin(maxbins=10)),
+        alt.Y('count()'),
+        tooltip = alt.Tooltip(['Driver_Age','count()'])
+    )
+)
+
+st.altair_chart(chart)
+
+
